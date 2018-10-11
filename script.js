b2446d765ad31c4dd181e628e214894223e0ec8f
Qi Lei <ricky@Qis-MacBook-Pro.local>
2018-10-11 07:48:53 -0600

    Create 3 new file

---
+++ script.js
@@ -0,0 +1,106 @@
+var yyy = document.getElementById('xxx')
+var context = yyy.getContext('2d')
+
+autoSetCanvasSize(yyy)
+listenToUser(yyy)
+
+var eraserEnabled = false
+eraser.onclick = function () {
+    eraserEnabled = true
+    actions.className = 'actions x'
+}
+brush.onclick = function () {
+    eraserEnabled = false
+    actions.className = 'actions'
+}
+
+
+/************ */
+function autoSetCanvasSize(canvas) {
+    setCanvasSize()
+
+    window.onresize = function () {
+        setCanvasSize()
+    }
+
+    function setCanvasSize() {
+        var pageWidth = document.documentElement.clientWidth
+        var pageHeight = document.documentElement.clientHeight
+
+        canvas.width = pageWidth
+        canvas.height = pageHeight
+    }
+}
+
+
+function drawCricle(x, y, radius) {
+    context.beginPath()
+    context.arc(x, y, radius, 0, Math.PI * 2)
+    context.fill()
+}
+
+function drawLine(x1, y1, x2, y2) { // x1y1 is begin, x2 y2 is end.
+    context.beginPath()
+    context.strokeStyle = 'black'
+    context.moveTo(x1, y1) // begin
+    context.lineWidth = 5
+    context.lineTo(x2, y2) // end  , 
+    context.stroke()
+    context.closePath()
+}
+
+function listenToUser(canvas) {
+    var using = false
+    var lastPoint = {x: undefined, y: undefined}
+
+    if (document.body.ontouchstart !== undefined) {
+        // Touch Device
+        console.log('touch OK')
+        console.log(document.body.ontouchstart)
+
+
+        canvas.ontouchstart = function () {
+            console.log('start touch')
+        }
+        canvas.ontouchmove = function () {
+            console.log('start move')
+        }
+        canvas.ontouchend = function () {
+            console.log('end')
+        }
+    } else {
+        canvas.onmousedown = function (aaa) {
+
+            var x = aaa.clientX
+            var y = aaa.clientY
+            using = true
+            if (eraserEnabled) {
+                context.clearRect(x - 5, y - 5, 10, 10)
+            } else {
+                lastPoint = {"x": x, "y": y}
+            }
+        }
+
+        canvas.onmousemove = function (aaa) {
+
+            var x = aaa.clientX
+            var y = aaa.clientY
+            if (!using) {
+                return
+            }
+            if (eraserEnabled) {
+                context.clearRect(x - 5, y - 5, 10, 10)
+            } else {
+                var newPoint = {"x": x, "y": y}
+                drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+                lastPoint = newPoint   // ***          
+            }
+        }
+        canvas.onmouseup = function (aaa) {
+            using = false
+        }
+
+
+    }
+
+}
